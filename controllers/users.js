39c332be8177f2455bf33f634cb9abd0f8418c88
Sushant Kumar <sushant.kumar@nowfloats.com>
2020-04-01 23:15:32 +0530

    secondcommit

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ let fs = require('fs');
 
 exports.GetUserList = (req, res) => {
   console.log(process.env.NODE_ENV);
-  if(process.env.NODE_ENV == "development") {
+  if(process.env.NODE_ENV == "production") {
     let object = fs.readFileSync("./controllers/mock-data.json", function(err, data) {
       if(err)
         throw err;
